2123d587313c0f725fa7df7471d42535dbdd3a33
Michele Longo <longomichele2015@gmail.com>
2024-11-11 21:30:57 +0100

    Added gtk and qt themes

--- user/home.nix
+++ user/home.nix
@@ -20,9 +20,52 @@
 
     pointerCursor = {
       gtk.enable = true;
-      package = pkgs.vanilla-dmz;
-      name = "Vanilla-DMZ";
+      name = "Numix-Cursor";
+      package = pkgs.numix-cursor-theme;
+    };
+
+    sessionVariables.GTK_THEME = "palenight";
+  };
+
+  gtk = {
+    enable = true;
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    theme = {
+      name = "palenight";
+      package = pkgs.palenight-theme;
+    };
+
+    cursorTheme = {
+      name = "Numix-Cursor";
+      package = pkgs.numix-cursor-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
     };
   };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
+    style = {
+      name = "gtk2";
+      package = pkgs.libsForQt5.breeze-qt5;
+    };
+  };
+
   programs.home-manager.enable = true;
 }
