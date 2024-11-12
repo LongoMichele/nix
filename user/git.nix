af1995ac048de85a3a19bfaf9d84c325938d988b
Michele Longo <longomichele2015@gmail.com>
2024-11-12 21:21:32 +0100

    Update git file

--- user/git.nix
+++ user/git.nix
@@ -1,11 +1,12 @@
 { config, pkgs, userSettings, ... }:
 
 {
-  home.packages = [ pkgs.git ];
-  programs.git.enable = true;
-  programs.git.userName = userSettings.description;
-  programs.git.userEmail = userSettings.email;
-  programs.git.extraConfig = {
-    init.defaultBranch = "master";
+  programs.git = {
+    enable = true;
+    userName = userSettings.description;
+    userEmail = userSettings.email;
+    extraConfig = {
+      init.defaultBranch = "master";
+    };
   };
 }
