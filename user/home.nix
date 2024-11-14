584b693f731b2c7844905695ba171bda0f303bb4
Michele Longo <longomichele2015@gmail.com>
2024-11-14 21:13:49 +0100

    Added node & Shopify cli to env pkgs

--- user/home.nix
+++ user/home.nix
@@ -13,6 +13,11 @@
 
   nixpkgs.config.allowUnfree = true;
   home = {
+    packages = with pkgs; [
+      nodejs_22
+      nodePackages_latest."@shopify/cli"
+    ];
+
     username = userSettings.username;
     homeDirectory = userSettings.home;
     stateVersion = "24.05";
