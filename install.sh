31d5029b038f483b901eaf52bcace711280ab6f7
Michele Longo <michele.longo@domain.com>
2025-01-26 23:24:33 +0100

    Installer update for non NixOS distros

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+if [ ! -f /etc/os-release ]; then
+  echo "Cannot find file /etc/os-release. Unknown OS, aborting."
+  exit 1
+fi
+DISTRO="$(cat /etc/os-release | grep -E -i '^NAME=' | cut -d '=' -f 2)"
+
+if [ -z $(command -v nix) ]; then
+  echo "Cannot find nix command. Nix is not installed, aborting."
+  exit 1
+fi
+
+if [ "$DISTRO" != "NixOS" ]; then
+  nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
+fi
+
 export NIX_CONFIG="experimental-features = nix-command flakes"
 
 # Install home-manager
